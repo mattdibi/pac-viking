fcce8dd427da346ce444f0de5a272f62535804d4
Mattia Dal Ben <matthewdibi@gmail.com>
2021-09-11 21:31:24 +0200

    Added Player scene and script, set player as main scene

---
+++ Player.gd
@@ -0,0 +1,45 @@
+extends Area2D
+
+
+# Declare member variables here.
+export var speed = 200  # How fast the player will move (pixels/sec).
+var screen_size  # Size of the game window.
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2()  # The player's movement vector.
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite.play()
+	else:
+		$AnimatedSprite.stop()
+	
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+	
+	if velocity.x != 0:
+		$AnimatedSprite.animation = "run_left"
+		$AnimatedSprite.flip_v = false
+		# See the note below about boolean assignment
+		$AnimatedSprite.flip_h = velocity.x > 0
+	elif velocity.y > 0:
+		$AnimatedSprite.animation = "run_front"
+	elif velocity.y < 0:
+		$AnimatedSprite.animation = "run_back"
+	
+	
