adaf57056f45ed3e73b5b0fbe7e93f1ff7f8b499
Mattia Dal Ben <matthewdibi@gmail.com>
2021-09-25 14:41:29 +0200

    Scripted standard coin spawn positions

--- Main.gd
+++ Main.gd
@@ -11,10 +11,24 @@ var power_coin_pos = [
 	Vector2(1520, 1520)
 ]
 
+# Standard Coins position array
+var std_coin_pos = [
+	Vector2(160, 80),
+	Vector2(160, 1520),
+	Vector2(1520, 160),
+	Vector2(1400, 1520)
+]
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Player.start($PlayerStartPosition.position)
-	$Coin.start($CoinStartPosition.position)
+
+	# Place coins in their positions
+	var std_coin = preload("res://Coin.tscn")
+	for coin_pos in std_coin_pos:
+		var coin = std_coin.instance()
+		add_child(coin)
+		coin.start(coin_pos)
 
 	# Place power coins in the four corners
 	var power_coin = preload("res://PowerCoin.tscn")
